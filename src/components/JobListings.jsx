396cb6b4decde6a6466520d84ddd259b2b44a691
Taine Jarvis <tainejarvis.dev@gmail.com>
2024-08-31 12:03:14 +0100

    changed colours

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -21,12 +21,12 @@ const JobListings = ({ isHome = false }) => {
     };
 
     fetchJobs();
-  }, []);
+  }, [isHome]);
 
   return (
     <section className='bg-blue-50 px-4 py-10'>
       <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+        <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:1967', // Ensure this matches your Express server port
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
