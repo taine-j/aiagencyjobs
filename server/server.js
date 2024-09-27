ccfe1a0ecc7237c454798b9af5ff004a607a47ae
Taine Jarvis <tainejarvis.dev@gmail.com>
2024-09-27 13:30:21 +0100

    added aiagencyjobs.com to cors policy

--- server/server.js
+++ server/server.js
@@ -44,10 +44,22 @@ app.use(express.static(path.join(__dirname, '../src')));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// Allowed origins array
+const allowedOrigins = ['http://localhost:3000', 'https://aiagencyjobs.com'];
+
 // Enable CORS
 app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true  // Allow cookies and other credentials to be sent
 }));
 
 // Configure authentication
